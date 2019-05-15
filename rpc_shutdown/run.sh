614f6604a217f0d99971494ac1e46d7fe6eb9374
Marlon <marlon.otto@outlook.de>
2019-05-15 20:29:28 +0200

    Update run.sh

--- rpc_shutdown/run.sh
+++ rpc_shutdown/run.sh
@@ -16,14 +16,18 @@ while read -r input; do
         ALIAS=$(jq --raw-output ".computers[$i].alias" $CONFIG_PATH)
         ADDRESS=$(jq --raw-output ".computers[$i].address" $CONFIG_PATH)
         CREDENTIALS=$(jq --raw-output ".computers[$i].credentials" $CONFIG_PATH)
-      
+        REBOOT=$(jq --raw-output ".computers[$i].reboot" $CONFIG_PATH)
+        MESSAGE=$(jq --raw-output ".computers[$i].message" $CONFIG_PATH)
+        TIME_TO_EXECUTE=$(jq --raw-output ".computers[$i].time_to_execute" $CONFIG_PATH)
+		
         # Not the correct alias
         if [ "$ALIAS" != "$input" ]; then
+		    echo 'continue...'
             continue
         fi
       
-        echo "[Info] Shutdown $input -> $ADDRESS"
-        if ! msg="$(net rpc shutdown -I "$ADDRESS" -U "$CREDENTIALS")"; then
+        echo "[Info] Shutdown $input -> $ADDRESS -> $MESSAGE -> $TIME_TO_EXECUTE -> $REBOOT
+        if ! msg="$(net rpc shutdown -I "$ADDRESS" -U "$CREDENTIALS" -C "$MESSAGE" -T "$TIME_TO_EXECUTE" "$REBOOT")"; then
             echo "[Error] Shutdown fails -> $msg"
         fi
     done
